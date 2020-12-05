2e864df5e03578602adea283dbe60abc2a511be2
RuLe0G <khlebnikov2015@yandex.ru>
2020-12-05 18:18:38 +0700

    Fin

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,7 +34,11 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // imageBox1
@@ -79,11 +83,49 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(658, 99);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            700,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(820, 12);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(640, 480);
+            this.imageBox2.TabIndex = 7;
+            this.imageBox2.TabStop = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(965, 504);
+            this.ClientSize = new System.Drawing.Size(1489, 504);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -91,6 +133,8 @@
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -102,6 +146,8 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private Emgu.CV.UI.ImageBox imageBox2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -22,6 +22,7 @@ namespace lab6
         Image<Gray, byte> bg = null;
         VideoCapture capture = new VideoCapture();
         private int framecount = 0;
+        public int Area = 700;
 
         BackgroundSubtractorMOG2 subtractor = new BackgroundSubtractorMOG2(1000, 32, true);
 
@@ -46,9 +47,9 @@ namespace lab6
         }
 
 
-
         private void timer1_Tick_1(object sender, EventArgs e)
         {
+
             framecount++;
             if (framecount >= capture.GetCaptureProperty(CapProp.FrameCount))
             {
@@ -59,12 +60,11 @@ namespace lab6
             {
                 var frame = capture.QueryFrame();
 
-
+                imageBox2.Image = Process2(frame);
                 imageBox1.Image = Process(frame);
-                //BG(cur);
+
 
             }
-
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -84,7 +84,7 @@ namespace lab6
         }
 
         private Image<Gray, byte> FilterMask(Image<Gray, byte> mask)
-        {
+        {            
             var anchor = new Point(-1, -1);
             var borderValue = new MCvScalar(1);
             // создание структурного элемента заданного размера и формы для морфологических операций
@@ -101,7 +101,34 @@ namespace lab6
             var threshold = dilation.ThresholdBinary(new Gray(240), new Gray(255));
             return threshold;
         }
+        private Image<Bgr, byte> Process2(Mat frame)
+        {
+            
+            Image<Gray, byte> cur = frame.ToImage<Gray, byte>();
+            Image<Gray, byte> diff = bg.AbsDiff(cur);
+                
+                diff.Erode(4);
+                diff.Dilate(6);
 
+                diff._ThresholdBinary(new Gray (10), new Gray(255));
+
+            VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+            CvInvoke.FindContours(diff, contours, null, RetrType.External,
+                ChainApproxMethod.ChainApproxTc89L1);
+            var output = frame.ToImage<Bgr, byte>().Copy();
+
+            for (int i = 0; i < contours.Size; i++)
+            {
+                if (CvInvoke.ContourArea(contours[i]) > Area)
+                {
+                    Rectangle boundingRect = CvInvoke.BoundingRectangle(contours[i]);
+                    output.Draw(boundingRect, new Bgr(Color.GreenYellow), 2);
+
+                }
+            }
+            //BG();
+            return output;
+        }
         private Image<Bgr, byte> Process(Mat frame)
         {
             Image<Gray, byte> cur = frame.ToImage<Gray, byte>();
@@ -120,7 +147,7 @@ namespace lab6
 
             for (int i = 0; i < contours.Size; i++)
             {
-                if (CvInvoke.ContourArea(contours[i]) > 700)
+                if (CvInvoke.ContourArea(contours[i]) > Area)
                 {
                     Rectangle boundingRect = CvInvoke.BoundingRectangle(contours[i]);
                     output.Draw(boundingRect, new Bgr(Color.GreenYellow), 2);
@@ -143,6 +170,11 @@ namespace lab6
             capture.Retrieve(frame);
 
             imageBox1.Image = Process(frame);
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            Area = Int32.Parse(numericUpDown1.Value.ToString());
         }
     }
 }
